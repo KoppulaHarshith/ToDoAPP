6b023fd602ac5fa3eb4582e1a183f81a7d77ae03
Koppula Harshith <koppulaharshith03@gmail.com>
2025-02-14 16:08:41 +0530

    Changed db url

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://todo_app_oyvv_user:rg7i2SlvQOfbip9oNGF0esIWpjWqfbi6@dpg-cunhm9ogph6c73et8m9g-a/todo_app_oyvv'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
